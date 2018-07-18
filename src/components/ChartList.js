7e7219aa6891296c768e261f7933b44d092d60b3
Luis Tejero <luisfaridta@gmail.com>
2018-07-18 10:19:26 +0100

    key and loading message added

--- src/components/ChartList.js
+++ src/components/ChartList.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const ChartList = (props) => {
-  if (props.songs.length === 0) return null;
+  if (props.songs.length === 0) return <h1> Loading</h1>;
   function cutLink (link) {
     let cutted = link.slice(14)
     let newLink = "https://embed.music" + cutted
@@ -9,12 +9,12 @@ const ChartList = (props) => {
     return newLink
   }
   const options = props.songs.map((song, index) => {
-  return <div>
+  return <div key ={index}>
     <h4> Position {index + 1} - {song['im:artist'].label} </h4>
     <ul>
     <li> Title: {song['im:name'].label}</li>
     <li> Release Date: {song['im:releaseDate'].attributes.label}</li>
-    <iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="150" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation" src={cutLink(song.id.label)} width="400"></iframe>
+    <iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="150" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation" src={cutLink(song.id.label)} width="60%"></iframe>
     </ul>
   </div>
 })
